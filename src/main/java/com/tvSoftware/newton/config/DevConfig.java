5b50d0c3a2d66b7093f366238c5db6074307e440
Thay-Oliveira96 <thaynan-rodrigues@outlook.com>
2022-04-14 13:59:29 -0300

    Banco de Desenvolvimento

--- src/main/java/com/tvSoftware/newton/config/DevConfig.java
+++ src/main/java/com/tvSoftware/newton/config/DevConfig.java
@@ -1,21 +1,31 @@
 package com.tvSoftware.newton.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.tvSoftware.newton.services.DBServices;
 
+/*
+ * Classe de configuração do banco de desenvolvimento
+ * */
 @Configuration
-@Profile("test")
-public class TesteConfig {
+@Profile("dev")
+public class DevConfig {
 	
 	@Autowired
 	private DBServices dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
 	@Bean
-	public void instanciaDB() {
-		this.dbService.instanciaDB();
+	public boolean instanciaDB() {
+		if(value.equals("create")) {
+			this.dbService.instanciaDB();
+		}
+		return false;
 	}
 }
