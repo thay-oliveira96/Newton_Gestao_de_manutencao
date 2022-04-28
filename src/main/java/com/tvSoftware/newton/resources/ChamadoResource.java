36d47224db2bd8a1bad1f1f8ed9fa047291a1402
Thay-Oliveira96 <thaynan-rodrigues@outlook.com>
2022-04-28 15:25:43 -0300

    findById das Ordens de Manutenção

---
+++ src/main/java/com/tvSoftware/newton/resources/ChamadoResource.java
@@ -0,0 +1,26 @@
+package com.tvSoftware.newton.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tvSoftware.newton.domain.Chamado;
+import com.tvSoftware.newton.domain.dtos.ChamadoDTO;
+import com.tvSoftware.newton.services.ChamadoService;
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoResource {
+
+	@Autowired
+	private ChamadoService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
+		Chamado obj = service.findById(id);
+		return ResponseEntity.ok().body(new ChamadoDTO(obj));
+	}
+}
---
+++ src/main/java/com/tvSoftware/newton/services/ChamadoService.java
@@ -0,0 +1,23 @@
+package com.tvSoftware.newton.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tvSoftware.newton.domain.Chamado;
+import com.tvSoftware.newton.repositories.ChamadoRepository;
+import com.tvSoftware.newton.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class ChamadoService {
+
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID:" + id));
+	}
+		
+}
